24974a7413944b600e4e33b96f80b982d767f15b
Lagmull <156221691+Lagmull@users.noreply.github.com>
2024-01-11 17:43:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -38,6 +38,72 @@ bot.on('message', async (ctx, next) => {
               const maxRetries = 10;
       while (retryCount < maxRetries) {
         try {
+          const resp2 = await fetch("https://addlivetag.com/", {
+            "headers": {
+              "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
+              "accept-language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
+              "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\"",
+              "sec-ch-ua-mobile": "?1",
+              "sec-ch-ua-platform": "\"Android\"",
+              "sec-fetch-dest": "document",
+              "sec-fetch-mode": "navigate",
+              "sec-fetch-site": "same-origin",
+              "sec-fetch-user": "?1",
+              "upgrade-insecure-requests": "1",
+              "cookie": "us_id=8ce0d9b891645b8162e72b76a2c8e50d; PHPSESSID=l8hakk8vsgopv8sr7bg737136o; user=xurio; ref=xurio; _ga=GA1.1.1952564566.1704787707; _fbp=fb.1.1704787706854.147812631; ref=0; _ga_JFEPJSWCC6=GS1.1.1704787706.1.1.1704789018.0.0.0",
+              "Referer": "https://addlivetag.com/",
+              "Referrer-Policy": "strict-origin-when-cross-origin"
+            },
+            "body": null,
+            "method": "GET"
+          })
+          const res2 = await resp2.text()
+          const startIndex = res2.indexOf('<td>1</td>')
+          const endIndex = res2.lastIndexOf(url)
+          const content = res2.substring(startIndex, endIndex).toString()
+          const linkRegex2 = /href="(.*?)"/;
+          //const long = /https:\/\/vn.shp.ee\//
+          
+          if (content.match(/data-id="(\d+)"/) && content.match(/user-id="([^"]+)"/)) {
+          console.log("get")
+          await fetch("https://addlivetag.com/api/view-video.php", {
+                "headers": {
+                  "accept": "application/json, text/javascript, */*; q=0.01",
+                  "accept-language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
+                  "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
+                  "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\"",
+                  "sec-ch-ua-mobile": "?1",
+                  "sec-ch-ua-platform": "\"Android\"",
+                  "sec-fetch-dest": "empty",
+                  "sec-fetch-mode": "cors",
+                  "sec-fetch-site": "same-origin",
+                  "x-requested-with": "XMLHttpRequest",
+                  "cookie": `us_id=${userId[1]}; PHPSESSID=l8hakk8vsgopv8sr7bg737136o; user=xurio; ref=xurio; _ga=GA1.1.1952564566.1704787707; _fbp=fb.1.1704787706854.147812631; ref=0; _ga_JFEPJSWCC6=GS1.1.1704787706.1.1.1704794566.0.0.0`,
+                  "Referer": "https://addlivetag.com/",
+                  "Referrer-Policy": "strict-origin-when-cross-origin"
+                },
+                "body": `user_id=${userId[1]}&id=${videoId[1]}`,
+                "method": "POST"
+              })
+           
+              console.log("thanhcong")
+            const lH = content.match(linkRegex2)[1]  
+           const video = await lH.split("?")[0]
+           const sVideo = await fetch(video)
+          const affLink = await `https://shope.ee/an_redir?origin_link=${encodeURIComponent(sVideo.url.split("?")[0])}&affiliate_id=17384020006&sub_id=tagsVideo`
+           const strMess = `<b>✅ Đã Gắn Video Thành Công</b> ${tagName}\n\n🚨 <i><b>Chú Ý</b>: Nếu Nhấp Link mà bị hiện <b>đen màn hình (video đã xoá)</b>. Vui lòng thoát hẳn app Shopee và thử lại!</i>\n\n<b>⏳ Tốc độ add Video từ 3-5 phút. Vui lòng chờ nha! Đừng cố gửi link thêm!</b>`
+          await ctx.replyWithPhoto(`https://i.pinimg.com/564x/38/ab/9a/38ab9a08d13fac26b9a40083390c1058.jpg`,{caption: strMess, message_thread_id: threadID, reply_markup: {
+                    inline_keyboard: [
+                      /* Inline buttons. 2 side-by-side */
+                      [ { text: "💯 Đến Video 💯", url: affLink }, { text: "💯 Add Live 💯", url: "https://t.me/CoNenChotDon/1464" }],
+          
+                      /* One button */
+                      //[ { text: "❓Hướng Dẫn", url: "https://t.me/ChotDonBot" }, { text: "🔥 15 Voucher 50K", url: "https://www.facebook.com/groups/salelameofficial/"}]
+                  ]
+                }
+           , parse_mode: "HTML"});  
+           break;
+              }  
         const respee = await fetch(url)
         const resURL = await decodeURIComponent(respee.url.replace(/https:\/\/shopee\.vn\/universal-link\?af=false&deep_and_deferred=1&redir=/gm,''))
         //const peeDlink = resURL.match(/(.*?)\?/)[1]
@@ -83,74 +149,7 @@ bot.on('message', async (ctx, next) => {
     })
     console.log("add")
 
-    const resp2 = await fetch("https://addlivetag.com/", {
-      "headers": {
-        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-        "accept-language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
-        "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\"",
-        "sec-ch-ua-mobile": "?1",
-        "sec-ch-ua-platform": "\"Android\"",
-        "sec-fetch-dest": "document",
-        "sec-fetch-mode": "navigate",
-        "sec-fetch-site": "same-origin",
-        "sec-fetch-user": "?1",
-        "upgrade-insecure-requests": "1",
-        "cookie": "us_id=8ce0d9b891645b8162e72b76a2c8e50d; PHPSESSID=l8hakk8vsgopv8sr7bg737136o; user=xurio; ref=xurio; _ga=GA1.1.1952564566.1704787707; _fbp=fb.1.1704787706854.147812631; ref=0; _ga_JFEPJSWCC6=GS1.1.1704787706.1.1.1704789018.0.0.0",
-        "Referer": "https://addlivetag.com/",
-        "Referrer-Policy": "strict-origin-when-cross-origin"
-      },
-      "body": null,
-      "method": "GET"
-    })
-    const res2 = await resp2.text()
-    const startIndex = res2.indexOf('<td>1</td>')
-    const endIndex = res2.lastIndexOf(url)
-    const content = res2.substring(startIndex, endIndex).toString()
-    const linkRegex2 = /href="(.*?)"/;
-    const long = /https:\/\/vn.shp.ee\//
-    const lH = content.match(linkRegex2)[1]
-    const videoId = content.match(/data-id="(\d+)"/)
-    const userId = content.match(/user-id="([^"]+)"/)
-    const img = content.match(/data-src="(.*?)"/)[1]
-    if (videoId && userId) {
-    console.log("get")
-    await fetch("https://addlivetag.com/api/view-video.php", {
-          "headers": {
-            "accept": "application/json, text/javascript, */*; q=0.01",
-            "accept-language": "vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5",
-            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "sec-ch-ua": "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\"",
-            "sec-ch-ua-mobile": "?1",
-            "sec-ch-ua-platform": "\"Android\"",
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
-            "x-requested-with": "XMLHttpRequest",
-            "cookie": `us_id=${userId[1]}; PHPSESSID=l8hakk8vsgopv8sr7bg737136o; user=xurio; ref=xurio; _ga=GA1.1.1952564566.1704787707; _fbp=fb.1.1704787706854.147812631; ref=0; _ga_JFEPJSWCC6=GS1.1.1704787706.1.1.1704794566.0.0.0`,
-            "Referer": "https://addlivetag.com/",
-            "Referrer-Policy": "strict-origin-when-cross-origin"
-          },
-          "body": `user_id=${userId[1]}&id=${videoId[1]}`,
-          "method": "POST"
-        })
      
-        console.log("thanhcong")
-     const video = await lH.split("?")[0]
-     const sVideo = await fetch(video)
-    const affLink = await `https://shope.ee/an_redir?origin_link=${encodeURIComponent(sVideo.url.split("?")[0])}&affiliate_id=17384020006&sub_id=tagsVideo`
-     const strMess = `<b>✅ Đã Gắn Video Thành Công</b> ${tagName}\n\n🚨 <i><b>Chú Ý</b>: Nếu Nhấp Link mà bị hiện <b>đen màn hình (video đã xoá)</b>. Vui lòng thoát hẳn app Shopee và thử lại!</i>\n\n<b>⏳ Tốc độ add Video từ 3-5 phút. Vui lòng chờ nha! Đừng cố gửi link thêm!</b>`
-    await ctx.replyWithPhoto(`https://i.pinimg.com/564x/38/ab/9a/38ab9a08d13fac26b9a40083390c1058.jpg`,{caption: strMess, message_thread_id: threadID, reply_markup: {
-              inline_keyboard: [
-                /* Inline buttons. 2 side-by-side */
-                [ { text: "💯 Đến Video 💯", url: affLink }, { text: "💯 Add Live 💯", url: "https://t.me/CoNenChotDon/1464" }],
-    
-                /* One button */
-                //[ { text: "❓Hướng Dẫn", url: "https://t.me/ChotDonBot" }, { text: "🔥 15 Voucher 50K", url: "https://www.facebook.com/groups/salelameofficial/"}]
-            ]
-          }
-     , parse_mode: "HTML"});  
-     break;
-        } 
     }
       
     } catch (ers) {
