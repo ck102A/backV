d24ba430a26688f63071b9b3d44117eae7ae66ad
Lagmull <156221691+Lagmull@users.noreply.github.com>
2024-02-01 08:33:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ bot.on('message', async (ctx, next) => {
               "sec-fetch-site": "same-origin",
               "sec-fetch-user": "?1",
               "upgrade-insecure-requests": "1",
-              "cookie": "us_id=2689d678ca4bba4702d4629474c1401a; _ga=GA1.1.631115515.1706747169; _fbp=fb.1.1706747169312.813840167; ref=17387930011; user=17387930011; PHPSESSID=fdg19k6607kqh8k5t7l1no1ohr; _ga_JFEPJSWCC6=GS1.1.1706747169.1.1.1706747397.0.0.0",
+              "cookie": "us_id=8ce0d9b891645b8162e72b76a2c8e50d; PHPSESSID=l8hakk8vsgopv8sr7bg737136o; user=xurio; ref=xurio; _ga=GA1.1.1952564566.1704787707; _fbp=fb.1.1704787706854.147812631; ref=0; _ga_JFEPJSWCC6=GS1.1.1704787706.1.1.1704789018.0.0.0",
               "Referer": "https://addlivetag.com/",
               "Referrer-Policy": "strict-origin-when-cross-origin"
             },
@@ -79,7 +79,7 @@ bot.on('message', async (ctx, next) => {
                   "sec-fetch-mode": "cors",
                   "sec-fetch-site": "same-origin",
                   "x-requested-with": "XMLHttpRequest",
-                  "cookie": `us_id=${content.match(/user-id="([^"]+)"/)[1]}; _ga=GA1.1.631115515.1706747169; _fbp=fb.1.1706747169312.813840167; ref=17387930011; user=17387930011; PHPSESSID=fdg19k6607kqh8k5t7l1no1ohr; _ga_JFEPJSWCC6=GS1.1.1706747169.1.1.1706747397.0.0.0`,
+                  "cookie": `us_id=${content.match(/user-id="([^"]+)"/)[1]}; PHPSESSID=l8hakk8vsgopv8sr7bg737136o; user=xurio; ref=xurio; _ga=GA1.1.1952564566.1704787707; _fbp=fb.1.1704787706854.147812631; ref=0; _ga_JFEPJSWCC6=GS1.1.1704787706.1.1.1704794566.0.0.0`,
                   "Referer": "https://addlivetag.com/",
                   "Referrer-Policy": "strict-origin-when-cross-origin"
                 },
@@ -147,7 +147,7 @@ const tVideo = await rVideo.substring(startIndex2, endIndex2);
         "sec-fetch-mode": "cors",
         "sec-fetch-site": "same-origin",
         "x-requested-with": "XMLHttpRequest",
-        "cookie": "us_id=2689d678ca4bba4702d4629474c1401a; _ga=GA1.1.631115515.1706747169; _fbp=fb.1.1706747169312.813840167; ref=17387930011; user=17387930011; PHPSESSID=fdg19k6607kqh8k5t7l1no1ohr; _ga_JFEPJSWCC6=GS1.1.1706747169.1.1.1706747397.0.0.0",
+        "cookie": "us_id=8ce0d9b891645b8162e72b76a2c8e50d; ref=xurio; _ga=GA1.1.1952564566.1704787707; _fbp=fb.1.1704787706854.147812631; ref=0; PHPSESSID=gicfpeqkj9ddnq754lhpauuorm; user=xurio; _ga_JFEPJSWCC6=GS1.1.1704808576.5.1.1704808577.0.0.0",
         "Referer": "https://addlivetag.com/",
         "Referrer-Policy": "strict-origin-when-cross-origin"
       },
