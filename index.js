572f81305902c3b2bacf1a95a1353028dd9442c2
ck102A <160565502+ck102A@users.noreply.github.com>
2024-03-13 15:12:05 +0700

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ while (a<b) {
     if(used < 100){
         var d = new Date()
         console.log(used+"%" + ' time: ' + `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`)
-        await fetch('https://api.telegram.org/bot6043554925:AAGVcKddvm8t_nRzgBlclulBFynT0YwAN0s/sendMessage?chat_id=-1002139375881&text=' + used+"%" + ' time: ' + `${d.getHours()+7}:${d.getMinutes()}:${d.getSeconds() \n${info}`)
+        await fetch('https://api.telegram.org/bot6043554925:AAGVcKddvm8t_nRzgBlclulBFynT0YwAN0s/sendMessage?chat_id=-1002139375881&text=' + used+"%" + ' time: ' + `${d.getHours()+7}:${d.getMinutes()}:${d.getSeconds()} \n${info}`)
         break;
     } else {
         console.log(`running...`)
